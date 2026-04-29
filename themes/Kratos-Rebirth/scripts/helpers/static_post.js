afac718029a97d17336651fbffd8852663c87a6f
Suite <a471486944@gmail.com>
2026-04-29 12:10:23 +0800

    Remove directory from index to prepare for submodule re-addition

--- themes/Kratos-Rebirth/scripts/helpers/static_post.js
+++
@@ -1,5 +0,0 @@
-hexo.extend.helper.register('static_post', function(filename) {
-  if (!filename) return '';
-  const postName = this.page.source.split('/').pop().replace(/\.md$/, '');
-  return `/static/${postName}/${filename}`;
-});
